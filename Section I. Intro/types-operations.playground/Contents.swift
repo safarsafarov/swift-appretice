fd5647f424bb3ec92a4f983ac65e6cbcb4ee25c2
Safar Safarov <Safarov.safar@hotmail.com>
2022-12-02 15:23:19 +0500

    ✨

---
+++ Section I. Intro/types-operations.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+let wantADouble = 3
+//let acutallyDouble = Double(3)
+//let acutallyDouble: Double = 3
+//let acutallyDouble = 3 as Double
+
+//“Mini-exercises
+//Create a constant called age1 and set it equal to 42. Create a constant called age2 and set it equal to 21. Check using Option-click that the type for both has been inferred correctly as Int.”
+
+//let age1 = 42
+//let age2 = 2
+//let avg1 = (age1 + age2) / 2
+//
+//let days = (months: 12, days: 2, year: 2022, avarageTemperature: 12.4)
+//
+
+let tuple = (day: 15, month: 9, year: 2015)
+let day = tuple.day
